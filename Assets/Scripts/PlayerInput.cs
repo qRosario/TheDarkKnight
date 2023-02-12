7de0624f54cc6d095c4a4de3cd51f4fb90274c9e
Danya <danyastroev@mail.ru>
2023-02-12 19:18:01 +0300

    Fixed the character control system

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2.0f;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private LayerMask _notPlayer;
+    [SerializeField] private Transform _groundCheker;
+    private PlayerMovement _playerMovement;
+    private Vector3 _movement;
+    private Vector3 _moveDir;
+    private bool _jump;
+    private bool _isAir;
+
+    private void Start()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        PlayerReadInput();
+        _playerMovement.Animation(_movement, _isAir);
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMovement.Movement(_moveDir,_movement);
+        _playerMovement.Jump(_jump);
+    }
+
+    private void PlayerReadInput()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        _movement = new Vector3(horizontal, 0, vertical).normalized;
+        _movement = _camera.TransformDirection(_movement);
+        _moveDir = Vector3.ClampMagnitude(_movement, 1) * _speed;
+
+        if (Physics.Raycast(_groundCheker.position, Vector3.down, 0.1f, _notPlayer))
+        {
+            _isAir = false;
+
+            if (Input.GetKeyDown(KeyCode.Space) && _isAir == false)
+            {
+                _jump = true;
+            }
+        }
+        else
+        {
+            _isAir = true;
+            _jump = false;
+        }
+    }
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,50 +3,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 
 {
-    [SerializeField] private float _speed = 2.0f;
     [SerializeField] private float _rotationSpeed = 10.0f;
     [SerializeField] private float _JumpForce = 1.0f;
-    [SerializeField] private LayerMask _notPlayer;
-    [SerializeField] private Transform _groundCheker;
-    [SerializeField] private Transform _camera;
-    [SerializeField] private GameObject _audioHolder;
     private Animator _animator;
     private Rigidbody _player;
-    private Vector3 _movement;
-    private Vector3 _moveDir;
-    private bool _jump;
-    private bool _isAir;
     
 
     private void Start()
     {
-        _animator = GetComponent<Animator>();
         _player = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
-    private void Update()
-    {
-        PlayerInput();
-        Animation();
-    }
-    private void FixedUpdate()
-    {
-        Movement();
-        Jump();
-    }
-
-
-    private void Jump()
+    public void Jump(bool _jump)
     {
         if (_jump == true)
         {
             _animator.SetTrigger("Jump");
             _player.AddForce(Vector3.up * _JumpForce, ForceMode.Impulse);
-            _jump = false;
         }
     }
 
-    private void Movement()
+    public void Movement(Vector3 _moveDir, Vector3 _movement)
     {
         _player.velocity = new Vector3(_moveDir.x, _player.velocity.y, _moveDir.z);
         _player.angularVelocity = Vector3.zero;
@@ -57,35 +35,12 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void PlayerInput()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        _movement = new Vector3(horizontal, 0, vertical).normalized;
-        _movement = _camera.TransformDirection(_movement);
-        _moveDir = Vector3.ClampMagnitude(_movement, 1) * _speed;
-
-        if (Physics.Raycast(_groundCheker.position, Vector3.down, 0.2f, _notPlayer))
-        {
-            _isAir = false;
-
-            if (Input.GetKeyDown(KeyCode.Space) && _isAir == false)
-            {
-                _jump = true;
-            }
-        }
-        else
-        {
-            _isAir = true;
-        }
-    }
-
-    private void Animation()
+    public void Animation(Vector3 _movement, bool _isAir)
     {
         _animator.SetFloat("MotionSpeed", Vector3.ClampMagnitude(_movement, 1).magnitude);
 
-        if (Physics.CheckSphere(_groundCheker.position, 0.2f, _notPlayer))
+        if (_isAir == false)
         {
             _animator.SetBool("IsAir", false);
         }
